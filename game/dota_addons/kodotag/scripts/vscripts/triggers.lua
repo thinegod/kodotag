19728d50fb74edd8de88314b11a684c7eefc3442
thinegod <magnus192@hotmail.com>
2014-09-01 14:27:58 +0200

    noda gary parouski

--- game/dota_addons/kodotag/scripts/vscripts/triggers.lua
+++ game/dota_addons/kodotag/scripts/vscripts/triggers.lua
@@ -1,7 +1,7 @@
 
 
 function startArea(keys)
-		print (keys.activator:GetGold())
+		print (keys.activator:GetGold())-- wat is dis
 end
 
 
@@ -23,15 +23,7 @@ function checkType(stuff)
 end
 
 function miningGold(keys)
-	--[[for i,v in ipairs(GameRules.KodoTagGameMode.goldMiners) do
-		if keys.activator == v.activator then
-			v.count=v.count+1
-			print ("PRINTEN "..v.count)
-			return nil
-		end
-	end]]
 	checkKeys(keys)
-	print (keys.caller:GetModelName())
 	local t = {["activator"]=keys.activator,["count"]=0,["goldMine"]=keys.caller:GetAbsOrigin()}--ska vara positionen av guldgruvan
 	table.insert(GameRules.KodoTagGameMode.goldMiners,t)
 end
@@ -46,18 +38,6 @@ function stopMiningGold(keys)
 	
 end
 
-function createCircularTrigger(keys)
-	local trigger=CreateTriggerRadiusApproximate(keys.target_points[1],keys.Radius)
-	trigger:Enable()
-	trigger.OnStartTouch=function(dfgh)
-		print("okgokfdsgok")
-	end
-	PrintCircle(keys.target_points[1],keys.Radius)
-
-end
-function test()
-print("that actually worked....")
-end
 function PrintSquare(v,size)
 			DebugDrawLine(Vector(v.x-size,v.y+size,BH_Z), Vector(v.x+size,v.y+size,BH_Z), 255, 0, 0, false, 30)
 			DebugDrawLine(Vector(v.x-size,v.y+size,BH_Z), Vector(v.x-size,v.y-size,BH_Z), 255, 0, 0, false, 30)
